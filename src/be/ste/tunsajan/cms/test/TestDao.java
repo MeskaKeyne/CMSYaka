634a21cefdc0c79f21c212e2b40bb1de5617b09a
Keyne <Keyne@tunsajan.eu>
2017-09-28 20:20:23 +0200

    init_test_repertories

---
+++ src/be/ste/tunsajan/cms/test/TestDao.java
@@ -0,0 +1,12 @@
+package be.ste.tunsajan.cms.test;
+
+import be.ste.tunsajan.cms.dao.Dao;
+
+public class TestDao {
+
+	public static void main(String[] args) {
+		Dao dao = new Dao("yaka_db");
+
+	}
+
+}
